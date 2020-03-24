bcaa65bcabc5dd5308b9b1533da88a36fc68be2d
shuidianbing <1226085282@qq.com>
2020-03-24 19:39:23 +0800

    添加glide绑定图片

---
+++ framework/src/main/java/com/stynet/framework/util/ImageViewResourcesAdapter.java
@@ -0,0 +1,10 @@
+package com.stynet.framework.util;
+
+/**
+ * Created by shuiDianBing on 19:32.
+ * Refer to the website << nullptr
+ * QQ << 1226085282 &  Email << 1226085282@qq.com
+ * function <<
+ */
+public class ImageViewResourcesAdapter {
+}
